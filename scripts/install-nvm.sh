c3539aa984e6490feaf6a523382bdef1411abb86
Wiesław Kędzierski <w.m.kedzierski@gmail.com>
2025-09-13 14:40:48 +0200

    Install nvm - nodejs installer.

---
+++ scripts/install-nvm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+
+# After instalation
+# Use
+# source ~/.bashrc
+#
+# nvm install --lts - it will install latest LTS version of node.
+#
+# nvm install 22 - it will install version 22
+#
+# node -v - verify installed version
